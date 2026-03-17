ec14120bc89f83956c04460aaee67acac544be32
shashanklearner23 <shashanklearner23@gmail.com>
2026-03-17 16:10:31 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ai-hub-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
